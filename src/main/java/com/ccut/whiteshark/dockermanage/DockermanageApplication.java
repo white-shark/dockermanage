590cf1c752a28d532f6216353df8fba027a0a4d9
white-shark <727789136@qq.com>
2021-03-27 14:45:23 +0800

    创建项目

---
+++ src/main/java/com/ccut/whiteshark/dockermanage/DockermanageApplication.java
@@ -0,0 +1,13 @@
+package com.ccut.whiteshark.dockermanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockermanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockermanageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ccut/whiteshark/dockermanage/DockermanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccut.whiteshark.dockermanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockermanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
